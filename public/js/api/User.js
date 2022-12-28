67d8575d301fd2460135f87c4e2a93a0784c119f
NeuroK-hub <102793426+NeuroK-hub@users.noreply.github.com>
2022-12-28 21:54:46 +0300

    Add files via upload

--- public/js/api/User.js
+++ public/js/api/User.js
@@ -1,77 +1,79 @@
-/**
- * Класс User управляет авторизацией, выходом и
- * регистрацией пользователя из приложения
- * Имеет свойство URL, равное '/user'.
- * */
+
 class User {
-  /**
-   * Устанавливает текущего пользователя в
-   * локальном хранилище.
-   * */
+  
+  static URL = '/user';
+
   static setCurrent(user) {
-
+    localStorage.user = JSON.stringify(user);
   }
 
-  /**
-   * Удаляет информацию об авторизованном
-   * пользователе из локального хранилища.
-   * */
   static unsetCurrent() {
-
+    delete localStorage.user;
   }
 
-  /**
-   * Возвращает текущего авторизованного пользователя
-   * из локального хранилища
-   * */
+
   static current() {
-
+      return JSON.parse(localStorage.getItem('user'));
   }
 
-  /**
-   * Получает информацию о текущем
-   * авторизованном пользователе.
-   * */
   static fetch(callback) {
-
+    const xhr = createRequest({
+       url: this.URL + '/current',
+       method: 'GET',
+       callback: (err, response) => {
+          if (response.success) {
+             User.setCurrent(response.user);
+          } else {
+             User.unsetCurrent();
+          }
+           callback(err, response);
+      }
+    });
   }
 
-  /**
-   * Производит попытку авторизации.
-   * После успешной авторизации необходимо
-   * сохранить пользователя через метод
-   * User.setCurrent.
-   * */
-  static login(data, callback) {
-    createRequest({
+  
+  
+
+  static login( data, callback) {
+   const xhr = createRequest({
       url: this.URL + '/login',
       method: 'POST',
-      responseType: 'json',
       data,
       callback: (err, response) => {
-        if (response && response.user) {
-          this.setCurrent(response.user);
+        if (response.success) {
+          User.setCurrent(response.user);
         }
         callback(err, response);
       }
     });
   }
 
-  /**
-   * Производит попытку регистрации пользователя.
-   * После успешной авторизации необходимо
-   * сохранить пользователя через метод
-   * User.setCurrent.
-   * */
-  static register(data, callback) {
-
+  static register( data, callback) {
+     let xhr = createRequest({
+       url: this.URL + '/register',
+       method: 'POST',
+       data,
+       callback: (err, response) => {
+          if (response.success) {
+             User.setCurrent(response.user);
+          };
+           callback(err, response);
+      }
+    });
   }
 
-  /**
-   * Производит выход из приложения. После успешного
-   * выхода необходимо вызвать метод User.unsetCurrent
-   * */
-  static logout(callback) {
 
+  static logout( data, callback) {
+     let xhr = createRequest({
+       url: this.URL + '/logout',
+       method: 'POST',
+       data,
+       callback: (err, response) => {
+          if (response.success) {
+             User.unsetCurrent();
+          };
+           callback(err, response);
+      }
+    });
   }
 }
--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -1,7 +1,33 @@
-/**
- * Основная функция для совершения запросов
- * на сервер.
- * */
-const createRequest = (options = {}) => {
 
-};
+const createRequest = (options = {}) => {
+  
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    let url = options.url;
+    let dataToSend = null;
+
+    if (options.method === 'GET') {
+      if (options.data) {
+         url = options.url + '?';
+         for (let prop in options.data) {
+            url += prop + '=' + options.data[prop] + '&'
+          }
+         url = url.substr(0, url.length - 1);
+      }
+
+    } else {
+        dataToSend = new FormData();
+        for (let prop in options.data) {
+          dataToSend.append(prop, options.data[prop]);
+        }
+    }
+  
+    try {
+        xhr.open(options.method, url);
+        xhr.send(dataToSend);  
+    } catch (err) {
+        options.callback(err); 
+    }
+    
+    xhr.onload = () => { options.callback(null, xhr.response) };
+}
